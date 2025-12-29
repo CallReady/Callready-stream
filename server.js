5549c0ec9f707f02b8327f419354c09deaf0ee63
CallReady <bradreedthompson@gmail.com>
2025-12-29 10:29:19 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,21 @@ app.get("/", (req, res) => {
   res.type("text/plain").send("CallReady stream server is running.");
 });
 
+// Twilio will request this URL when a call comes in.
+// It returns TwiML that tells Twilio to start streaming audio to our WebSocket URL.
+app.post("/twiml", (req, res) => {
+  const twiml =
+    `<?xml version="1.0" encoding="UTF-8"?>` +
+    `<Response>` +
+    `<Say>Connecting you now.</Say>` +
+    `<Connect>` +
+    `<Stream url="wss://callready-stream.onrender.com/stream" />` +
+    `</Connect>` +
+    `</Response>`;
+
+  res.type("text/xml").send(twiml);
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Listening on port " + port);
