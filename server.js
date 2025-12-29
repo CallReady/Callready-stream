d2d194ae021e36396f5c6f53cfe4bc15c87914d1
CallReady <bradreedthompson@gmail.com>
2025-12-29 10:30:27 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
 
 const app = express();
+app.use(express.urlencoded({ extended: false }));
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 app.get("/", (req, res) => {
   res.type("text/plain").send("CallReady stream server is running.");
 });
 
-// Twilio will request this URL when a call comes in.
-// It returns TwiML that tells Twilio to start streaming audio to our WebSocket URL.
 app.post("/twiml", (req, res) => {
   const twiml =
     `<?xml version="1.0" encoding="UTF-8"?>` +
     `<Response>` +
-    `<Say>Connecting you now.</Say>` +
+    `<Say>Thank you for calling CallReady.</Say>` +
     `<Connect>` +
     `<Stream url="wss://callready-stream.onrender.com/stream" />` +
     `</Connect>` +
@@ -21,7 +25,19 @@ app.post("/twiml", (req, res) => {
   res.type("text/xml").send(twiml);
 });
 
+wss.on("connection", (ws, req) => {
+  console.log("WebSocket connected");
+
+  ws.on("message", (data) => {
+    console.log("Received stream data");
+  });
+
+  ws.on("close", () => {
+    console.log("WebSocket closed");
+  });
+});
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log("Listening on port " + port);
 });
