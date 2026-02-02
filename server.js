469da4cac7d0d868d4a72beb84d01cdaa36d74f5
CallReady <bradreedthompson@gmail.com>
2026-02-01 20:21:03 -0800

    tweaking turn detection settings

--- server.js
+++ server.js
@@ -2684,8 +2684,8 @@ closeAll("Redirect to /unavailable failed");
               turn_detection: {
                 type: "server_vad",
                 silence_duration_ms: 650,
-                prefix_padding_ms: 400,
-                threshold: 0.35,
+                prefix_padding_ms: 500,
+                threshold: 0.30,
               },
             },
           });
