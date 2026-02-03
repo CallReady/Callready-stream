7fd2e10ebafa8ff74d405966d542a034dc57c323
CallReady <bradreedthompson@gmail.com>
2026-02-02 19:16:42 -0800

    fixing error

--- server.js
+++ server.js
@@ -2627,7 +2627,7 @@ closeAll("Redirect to /unavailable failed");
           "Ending rule:\n" +
           "If the human asks to end the call, quit, stop, hang up, or says they do not want to do this anymore, you must do both in the same response:\n" +
           "Say exactly: Okay, ending practice now.\n" +
-          "In text only, output exactly one line and nothing else: CALLREADY_END: END_CALL_NOW\n"
+          "In text only, output exactly one line and nothing else: CALLREADY_END: END_CALL_NOW\n",
             returnCallerBlock,
         },
       });
