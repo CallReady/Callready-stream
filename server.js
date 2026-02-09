98b54e72c08dd526bf22034bc351ce2edec9a7a6
CallReady <bradreedthompson@gmail.com>
2026-02-08 20:12:12 -0800

    Fix ws import so WebSocket.Server works on Node 22

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 
 const express = require("express");
 const http = require("http");
+const WebSocket = require("ws");
 const twilio = require("twilio");
 const { Pool } = require("pg");
 const Stripe = require("stripe");
