0c14c02409f4c1acc2be94bf53b3746260117b9f
CallReady <bradreedthompson@gmail.com>
2026-02-18 18:18:14 -0800

    changes

--- server.js
+++ server.js
@@ -3750,7 +3750,6 @@ wss.on("connection", (twilioWs) => {
           turnDetectionEnabled &&
           callState.phase === "choose_scenario" &&
           callState.scenarioConfirmCaptureInFlight &&
-          callState.subphase === "scenario_auto_pick_confirm" &&
           sawCallerSpeechSinceLastAIDone
         ) {
 
