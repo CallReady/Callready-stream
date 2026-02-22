fc55e0255e05bfe89c292d8a0a9a46f9f1c82749
CallReady <bradreedthompson@gmail.com>
2026-02-22 11:05:51 -0800

    changing 800 to .8

--- server.js
+++ server.js
@@ -1960,7 +1960,7 @@ app.post("/gather-choose-scenario", async (req, res) => {
     const gather = vr.gather({
       input: "speech",
       timeout: 3,
-      speechTimeout: 800,
+      speechTimeout: 0.8,
       action: "/process-choose-scenario",
       method: "POST",
       language: "en-US"
@@ -2013,7 +2013,7 @@ app.all("/gather-previous-scenario", async (req, res) => {
     const gather = vr.gather({
       input: "speech",
       timeout: 5,
-      speechTimeout: 800,
+      speechTimeout: 0.8,
       action: "/process-previous-scenario",
       method: "POST",
       language: "en-US"
